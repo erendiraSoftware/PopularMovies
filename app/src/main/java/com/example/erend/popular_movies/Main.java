04a876ac2cdbd667396f16b2cc2f99316ba1a35e
Erendira ATM <altoera@gmail.com>
2017-03-04 11:21:12 -0600

    Primer versión

--- app/src/main/java/com/example/erend/popular_movies/Main.java
+++ app/src/main/java/com/example/erend/popular_movies/Main.java
@@ -2,14 +2,19 @@ package com.example.erend.popular_movies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class Main extends AppCompatActivity {
-    private String[] data = new String[]{
+    private String[] datos = new String[]{
             "anarillo", "rojo verde", "azul"
-    }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayAdapter<String> adaptador= new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, datos);
+        ListView lista = (ListView)findViewById(R.id.listaDatos);
+        lista.setAdapter(adaptador);
     }
 }
